ad6a52a0457cbfed427535eaa769e2a00a000302
Mauricio Codeguez <mauricio.codeguez@gmail.com>
2021-05-19 00:08:46 -0300

    Adicionando juros controller

---
+++ CalculaJuros.Api/Controllers/JurosController.cs
@@ -0,0 +1,21 @@
+﻿using CalculaJuros.Domain.Interfaces.Services;
+using CalculaJuros.Domain.Queries;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CalculaJuros.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JurosController : ControllerBase
+    {
+        private readonly ICalculaJurosService _calculaJurosService;
+
+        public JurosController(ICalculaJurosService calculaJurosService) => _calculaJurosService = calculaJurosService;
+
+        [HttpGet]
+        [Route("calculaJuros")]
+        public async Task<CalculaJurosQuery> Get([FromQuery] double valorInicial, [FromQuery] int meses)
+            => await _calculaJurosService.CalcularJuros(valorInicial, meses);
+    }
+}
